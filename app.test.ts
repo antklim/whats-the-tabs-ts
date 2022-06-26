361a1b7d2aaa4c46e526b8f410005f9e30480306
Anton Klimenko <antklim@gmail.com>
2022-06-25 21:34:53 -0700

    Correlation Id middleware added

--- app.test.ts
+++ app.test.ts
@@ -1,9 +1,20 @@
 import { agent as request } from "supertest"
 import app from "./app"
 
-describe("App default settings", () => {
-  test("it responses with 404 Not Found for unknown routes", async () => {
-    const res = await request(app.callback()).get("/unknown")
-    expect(res.statusCode).toBe(404)
+describe("App defaults", () => {
+  test("app responses with 404 Not Found for unknown routes", async () => {
+    await request(app.callback()).get("/unknown").expect(404)
+  })
+
+  describe("correlation ID", () => {
+    test("app keeps provided correlation ID", async () => {
+      const res = await request(app.callback()).get("/info").set("X-Correlation-Id", "my-correlation-id")
+      expect(res.get("X-Correlation-Id")).toBe("my-correlation-id")
+    })
+
+    test("app adds correlation ID", async () => {
+      const res = await request(app.callback()).get("/info")
+      expect(res.get("X-Correlation-Id")).toBeDefined()
+    })
   })
 })
--- app.ts
+++ app.ts
@@ -1,6 +1,24 @@
 import Koa from "koa"
+import { v4 as uuid } from "uuid"
+
+import infoRouter from "./routes/info"
 
 const app = new Koa()
+
+app.use(async (ctx, next) => {
+  const correlationIdheaderName = "x-correlation-id"
+
+  let correlationId = ctx.request.get(correlationIdheaderName)
+  if (correlationId === "") {
+    correlationId = uuid()
+  }
+
+  ctx.set(correlationIdheaderName, correlationId)
+
+  await next()
+})
+
+app.use(infoRouter.routes())
 
 app.use((ctx) => {
   ctx.throw(404)
