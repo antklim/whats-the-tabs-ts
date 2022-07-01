4b855c2a0d98721ba53cb91a4518be658ce3b2f8
Anton Klimenko <antklim@gmail.com>
2022-07-01 15:56:54 +1000

    load from file added to in memory repo

--- songs/repo.memory.ts
+++ songs/repo.memory.ts
@@ -1,3 +1,4 @@
+import { readFile } from "node:fs/promises"
 import { Song, SongsRepository } from "."
 
 export class InMemorySongsRepository implements SongsRepository {
@@ -23,6 +24,13 @@ export class InMemorySongsRepository implements SongsRepository {
   songsByArtist(pattern: string, limit = 10) {
     const re = new RegExp(pattern, "i")
     return this.songs.filter(({ artist }) => re.test(artist)).slice(0, limit)
+  }
+
+  // loads data from file
+  async load(path: string) {
+    const rawJson = await readFile(path)
+    const songs: Song[] = JSON.parse(rawJson.toString("utf8"))
+    this.addSongs(songs)
   }
 
   purge() {
